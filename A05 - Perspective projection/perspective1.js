326490c4a32cd72b388ee902c599fb6f669919d7
andrea-cola <andrea.cola95@gmail.com>
2019-05-18 22:58:31 +0200

    Fifth assignment done.

--- A05 - Perspective projection/perspective1.js
+++ A05 - Perspective projection/perspective1.js
@@ -1,9 +1,15 @@
 function perspective() {
 	// Build a perspective projection matrix, for a 16/9 viewport, with fov-y=90, near plane n=0.1, and far plane f=100.
-	var out = [1.0,		0.0,		0.0,		0.0,
-			   0.0,		1.0,		0.0,		0.0,
-			   0.0,		0.0,		1.0,		0.0,
-			   0.0,		0.0,		0.0,		1.0];
+	var a = 16/9;
+	var angle = Math.PI;
+	var t = Math.tan(angle/2);
+	var n = 0.1;
+	var f = 100;
+
+	var out = [1.0 / (a*t),		0.0,		0.0,		0.0,
+			   0.0,		1.0 / t,		0.0,			0.0,
+			   0.0,		0.0,		(f+n) / (n-f),		2*f*n/(n-f),
+			   0.0,		0.0,		-1.0,		0.0];
 
 	return out;
 }
--- A05 - Perspective projection/perspective2.js
+++ A05 - Perspective projection/perspective2.js
@@ -1,9 +1,17 @@
 function perspective(w, h, fov) {
 	// Build a perspective projection matrix, for a viewport whose size is determined by parameters w (width) and h (height), and whose fov-y is passed in parameter fov. Near plane is n=0.1, and far plane f=100.
-	var out = [1.0,		0.0,		0.0,		0.0,
-			   0.0,		1.0,		0.0,		0.0,
-			   0.0,		0.0,		1.0,		0.0,
-			   0.0,		0.0,		0.0,		1.0];
+	var angle = fov * Math.PI / 180;
+	var a = w/h;
+	var t = Math.tan(angle/2);
+	var n = 0.1;
+	var f = 100.0;
+
+	console.log(angle + " " + t);
+
+	var out = [1.0/(a*t),		0.0,		0.0,		0.0,
+			   0.0,		1.0/t,		0.0,			0.0,
+			   0.0,		0.0,		(f+n)/(n-f),		2*f*n/(n-f),
+			   0.0,		0.0,		-1.0,		1.0];
 
 	return out;
 }
